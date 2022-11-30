16e9e36687b56fc0948dc8242fa2057a8b3c83b3
santiQ <santiagossc@live.com.ar>
2022-11-30 01:25:02 -0300

    Models init modified

--- cuTradeNet/Models/__init__.py
+++ cuTradeNet/Models/__init__.py
@@ -1,3 +1,30 @@
+'''
+For creating a new model use:
+> Models.ModelName(*args,**kwargs)
+where ModelName is the name of the model and *args and **kwargs are the arguments of the model.
+
+Or import the model and use:
+> from cuTradeNet.Models import ModelName
+> ModelName(*args,**kwargs)
+
+Example:
+> from cuTradeNet import Models
+> S=Models.YardSale(G=g,f=0.3)
+
+List of avaliable models by name:
+* YardSale - Yard Sale model
+* MergerSpinoff - Merger Spinoff model
+* Constant - Constant exchange model
+* DragulescuYakovenko - Drăgulescu and Yakovenko model
+* ChakrabortiChakrabarti -  Chakraborti and Chakrabarti model
+* Allin - "All in" trade model
+
+Especifications of the models at:
+https://github.com/Qsanti/cuTradeNet/blob/master/Models%26Demo/ModelsList.ipynb
+
+'''
+
+
 from .YardSale import YSNetModel as YardSale
 from .MergerSpinoff import MSNetModel as MergerSpinoff
 from .Constant import CNetModel as Constant
